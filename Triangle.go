0ba251b20229d441324d988ec290aaaa73a2dc88
Kevin Jayne <Kevin.Jayne@ADP.com>
2017-09-04 13:43:54 -0400

    static triangel points and vao

--- Triangle.go
+++ Triangle.go
@@ -5,21 +5,28 @@ import "github.com/go-gl/gl/v4.1-core/gl"
 // Triangle : a struct to hold openGL triangle data
 type Triangle struct {
 	Points []float32
-	Vao    uint32
+	Vao    *uint32
 }
 
-// New : Create a new Triangle
-func (t Triangle) New() Triangle {
-	points := []float32{
+var (
+	vao    uint32
+	points = []float32{
 		0, 0.5, 0,
 		-0.5, -0.5, 0,
 		0.5, -0.5, 0,
 	}
-	return Triangle{points, makeVao(points)}
+)
+
+// New : Create a new Triangle
+func (t Triangle) New() Triangle {
+	if vao == 0 {
+		vao = makeVao(points)
+	}
+	return Triangle{points, &vao}
 }
 
 // Draw : draw the triangle
 func (t Triangle) Draw() {
-	gl.BindVertexArray(t.Vao)
+	gl.BindVertexArray(*t.Vao)
 	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(t.Points)/3))
 }
