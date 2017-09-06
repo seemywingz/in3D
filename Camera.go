f4d4e93e44657bce7a555c3e050841cf8194e9bd
Kevin Jayne <Kevin.Jayne@ADP.com>
2017-09-05 23:58:02 -0400

    playing with view

--- Camera.go
+++ Camera.go
@@ -19,9 +19,10 @@ type Camera struct {
 func (c *Camera) Update() {
 	// c.Z -= 0.1
 	// c.X -= 0.1
+
 	translateMatrix := mgl32.Translate3D(c.X, c.Y, c.Z)
 	model := translateMatrix.Mul4(c.Model)
-	// TODO: update MVP including camera rotation i.e: c.MVP = c.Projection.Mul4(view.Mul4(model))
+	// TODO: update view from camera rotation
 	c.MVP = c.Projection.Mul4(c.View.Mul4(model))
 	gl.UniformMatrix4fv(0, 1, false, &c.MVP[0])
 }
@@ -33,15 +34,17 @@ func (c Camera) New(position Position) Camera {
 	defer free()
 	mvpid := gl.GetUniformLocation(shaders[0], *mvPointer)
 
-	view := mgl32.LookAt(
-		0, 0, 0, //Camera is at (0, 0, -1), in world space
-		0, 0, 0, //and looks at the origin
-		0, 1, 0, //head is up (set to 0, -1, 0 to look upside-down)
-	)
-
 	//Projection matrix : 45° Field of View, width:height ratio, display range : 0.1 unit <-> 100 units
 	projection := mgl32.Perspective(mgl32.DegToRad(45.0), width/height, 0.1, 100)
 	//model matrix : and identity matrix (model will be at te origin)
+	model := mgl32.Ident4()
+	view := mgl32.Ident4()
+	// view := mgl32.LookAt(
+	// 	c.X, c.Y, c.Z, //Camera is at (x, y, z), in world space
+	// 	0, 0, 0, //and looks at the origin
+	// 	0, 1, 0, //head is up (set to 0, -1, 0 to look upside-down)
+	// )
+	mvp := projection.Mul4(view.Mul4(model))
 
-	return Camera{projection, mgl32.Ident4(), mgl32.Ident4(), view, mvpid, position}
+	return Camera{projection, model, view, mvp, mvpid, position}
 }
