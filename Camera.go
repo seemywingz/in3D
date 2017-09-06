c1c33f309bea091106ae5359c9a97a1ff3249d5f
Kevin Jayne <Kevin.Jayne@ADP.com>
2017-09-06 01:44:52 -0400

    move controls to camera

--- Camera.go
+++ Camera.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.2/glfw"
 	"github.com/go-gl/mathgl/mgl32"
 )
 
@@ -18,10 +22,57 @@ type Camera struct {
 var (
 	xangle float32
 	yangle float32
+	lastX  float64
+	lastY  float64
 )
+
+// MouseControls : control the camera via the mouse
+func (Camera) MouseControls() {
+	x, y := window.GetCursorPos()
+	if lastX == 0 && lastY == 0 {
+		lastX = x
+		lastY = y
+	}
+	yangle += -float32(lastX - x)
+	xangle += -float32(lastY - y)
+	if window.GetMouseButton(glfw.MouseButton1) == glfw.Press {
+		fmt.Println("Click")
+	}
+	lastX = x
+	lastY = y
+}
+
+// KeyControls :
+func (c *Camera) KeyControls() {
+	if window.GetKey(glfw.KeyEscape) == glfw.Press {
+		os.Exit(1)
+	}
+	// Press w
+	if window.GetKey(glfw.KeyW) == glfw.Press {
+		camera.Z += 0.1
+	}
+	// Press A
+	if window.GetKey(glfw.KeyA) == glfw.Press {
+	}
+	// Press s
+	if window.GetKey(glfw.KeyS) == glfw.Press {
+		camera.Z -= 0.1
+	}
+	// Press d
+	if window.GetKey(glfw.KeyD) == glfw.Press {
+	}
+	// Press q
+	if window.GetKey(glfw.KeyQ) == glfw.Press {
+	}
+	// Press e
+	if window.GetKey(glfw.KeyE) == glfw.Press {
+	}
+}
 
 // Update : update camera
 func (c *Camera) Update() {
+	c.MouseControls()
+	c.KeyControls()
 	translateMatrix := mgl32.Translate3D(c.X, c.Y, c.Z)
 	model := translateMatrix.Mul4(c.Model)
 
--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"runtime"
 
 	"github.com/go-gl/gl/v4.1-core/gl"
@@ -51,8 +49,6 @@ func main() {
 	drawnObjects = append(drawnObjects, DrawnObjectData{}.New(Position{0, 0, 1}, Color{0, 0, 1}, triangle, shaders[0]))
 
 	for !window.ShouldClose() {
-		handleMouse()
-		handleKeys()
 		draw()
 	}
 }
@@ -64,52 +60,6 @@ func loadShaders() {
 			readShaderFile("./shaders/vertex.glsl"),
 			readShaderFile("./shaders/fragment.glsl"),
 		))
-}
-
-var (
-	lastX float64
-	lastY float64
-)
-
-func handleMouse() {
-	x, y := window.GetCursorPos()
-	if lastX == 0 && lastY == 0 {
-		lastX = x
-		lastY = y
-	}
-	yangle += -float32(lastX - x)
-	xangle += -float32(lastY - y)
-	if window.GetMouseButton(glfw.MouseButton1) == glfw.Press {
-		fmt.Println("Click")
-	}
-	lastX = x
-	lastY = y
-}
-
-func handleKeys() {
-	if window.GetKey(glfw.KeyEscape) == glfw.Press {
-		os.Exit(1)
-	}
-	// Press w
-	if window.GetKey(glfw.KeyW) == glfw.Press {
-		camera.Z += 0.1
-	}
-	// Press A
-	if window.GetKey(glfw.KeyA) == glfw.Press {
-	}
-	// Press s
-	if window.GetKey(glfw.KeyS) == glfw.Press {
-		camera.Z -= 0.1
-	}
-	// Press d
-	if window.GetKey(glfw.KeyD) == glfw.Press {
-	}
-	// Press q
-	if window.GetKey(glfw.KeyQ) == glfw.Press {
-	}
-	// Press e
-	if window.GetKey(glfw.KeyE) == glfw.Press {
-	}
 }
 
 func draw() {
