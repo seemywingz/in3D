fa300c9a21024ce69b4b0b8ccb6eaf9b20c99e89
Kevin Jayne <Kevin.Jayne@ADP.com>
2017-09-21 18:41:15 -0400

    use position

--- light.go
+++ light.go
@@ -15,7 +15,8 @@ type Light struct {
 
 // LightData :
 type LightData struct {
-	LPos  *float32
+	Position
+	// LPos  *float32
 	Iamb  *float32
 	Idif  *float32
 	Ispec *float32
@@ -30,7 +31,7 @@ func NewLight(pos, iamb, idif, ispec []float32) *Light {
 	IspecID := gl.GetUniformLocation(Shader["singleLight"], gl.Str("Ispec\x00"))
 
 	data := LightData{
-		&pos[0],
+		NewPosition(0, 0, 0),
 		&iamb[0],
 		&idif[0],
 		&ispec[0],
@@ -48,9 +49,8 @@ func NewLight(pos, iamb, idif, ispec []float32) *Light {
 // Draw :
 func (l *Light) Draw() {
 	gl.UseProgram(Shader["singleLight"])
-	gl.Uniform3fv(l.LPosID, 1, l.LPos)
+	gl.Uniform3fv(l.LPosID, 1, &[]float32{l.X, l.Y, l.Z}[0])
 	gl.Uniform3fv(l.IambID, 1, l.Iamb)
 	gl.Uniform3fv(l.IdifID, 1, l.Idif)
 	gl.Uniform3fv(l.IspecID, 1, l.Ispec)
-
 }
