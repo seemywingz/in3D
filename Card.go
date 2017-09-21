096d36c2f132f7dd7081414d85a879145bf555d5
Kevin Jayne <Kevin.Jayne@ADP.com>
2017-09-21 19:18:39 -0400

    comment card

--- Card.go
+++ Card.go
@@ -1,27 +1,27 @@
 package gg
 
-// Card :
-type Card struct {
-	Front *DrawnObject
-	Back  *DrawnObject
-}
-
-// NewCard : Create New Card
-func NewCard(p Position, ftexture, btexture, shader uint32) *Card {
-
-	cardfront := NewDrawnObject(p, CardFront, ftexture, shader)
-	cardback := NewDrawnObject(p, CardBack, btexture, shader)
-	cl := func(d *DrawnObject) {
-		// d.XRotation++
-		d.YRotation += 0.5
-	}
-	cardfront.DrawLogic = cl
-	cardback.DrawLogic = cl
-	return &Card{cardfront, cardback}
-}
-
-// Draw : draw the card (makes Card a SceneObject Interface)
-func (c *Card) Draw() {
-	c.Front.Draw()
-	c.Back.Draw()
-}
+// // Card :
+// type Card struct {
+// 	Front *DrawnObject
+// 	Back  *DrawnObject
+// }
+//
+// // NewCard : Create New Card
+// func NewCard(p Position, ftexture, btexture, shader uint32) *Card {
+//
+// 	cardfront := NewDrawnObject(p, CardFront, ftexture, shader)
+// 	cardback := NewDrawnObject(p, CardBack, btexture, shader)
+// 	cl := func(d *DrawnObject) {
+// 		// d.XRotation++
+// 		d.YRotation += 0.5
+// 	}
+// 	cardfront.DrawLogic = cl
+// 	cardback.DrawLogic = cl
+// 	return &Card{cardfront, cardback}
+// }
+//
+// // Draw : draw the card (makes Card a SceneObject Interface)
+// func (c *Card) Draw() {
+// 	c.Front.Draw()
+// 	c.Back.Draw()
+// }
