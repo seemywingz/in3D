02543a9eb968fefe52e5dba805730f17df47cca2
Kevin Jayne <Kevin.Jayne@ADP.com>
2017-09-26 16:44:30 -0400

    fire

--- examples/campFire/campFire.go
+++ examples/campFire/campFire.go
@@ -13,18 +13,18 @@ func main() {
 	gg.Enable(gg.PointerLock, true)
 	gg.Enable(gg.FlyMode, true)
 
-	fireColor := []float32{1, 0.5, 0}
-	fireLight := gg.NewLight()
-	fireLight.Difffuse = fireColor
-	fireLight.Position = gg.NewPosition(0, 40, 10)
-	fireLight.Radius = 15
-	fireLight.Draw = true
-
-	moonColor := []float32{1, 1, 1}
+	moonColor := []float32{0.3, 0.3, 0.8}
 	moon := gg.NewLight()
 	moon.Difffuse = moonColor
 	moon.Position = gg.NewPosition(100, 800, 0)
-	moon.Radius = 2000
+	moon.Radius = 1000
+
+	fireLight := gg.NewLight()
+	fireLight.Difffuse = []float32{10, 5, 1}
+	fireLight.Specular = []float32{1, 0, 0}
+	fireLight.Position = gg.NewPosition(0, 45, 10)
+	fireLight.Radius = 25
+	fireLight.Draw = true
 
 	objFile := "campFire"
 	gg.SetDirPath("github.com/seemywingz/gg/examples/assets/models/" + objFile)
