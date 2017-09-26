d32d0499bf8b137d3695e2f4d6042264c06b3dad
Kevin Jayne <Kevin.Jayne@ADP.com>
2017-09-25 23:50:30 -0400

    playing around

--- examples/mesh/mesh.go
+++ examples/mesh/mesh.go
@@ -10,17 +10,18 @@ func main() {
 
 	gg.Init(800, 600, "Wavefront Loader")
 	gg.SetCameraPosition(gg.NewPosition(0, 0.5, 2))
-	gg.Enable(gg.PointerLock, true)
-	gg.Enable(gg.FlyMode, true)
+	// gg.Enable(gg.PointerLock, true)
+	// gg.Enable(gg.FlyMode, true)
 
 	light := gg.NewLight([]float32{1, 1, 1})
 	light.Iamb = []float32{0.4, 0.4, 0.4}
-	light.Position = gg.NewPosition(-10, 100, 10)
+	light.Position = gg.NewPosition(-100, 100, 10)
 	light.Radius = 300
 
-	gg.SetDirPath("github.com/seemywingz/gg/examples/assets/models/trex")
+	objFile := "buddha"
+	gg.SetDirPath("github.com/seemywingz/gg/examples/assets/models/" + objFile)
 	// model is from: https://www.blendswap.com/blends/view/89437
-	mesh := gg.LoadObject("trex.obj")
+	mesh := gg.LoadObject(objFile + ".obj")
 	obj := gg.NewMeshObject(gg.Position{}, mesh, gg.Shader["phong"])
 	obj.SceneLogic = func(s *gg.SceneData) {
 		s.YRotation++
