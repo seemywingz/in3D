99971301a3bcab2d3929bd0da53634faba4e5839
Kevin Jayne <Kevin.Jayne@ADP.com>
2017-09-25 23:45:26 -0400

    add dino

---
+++ examples/mesh/mesh.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/seemywingz/gg"
+)
+
+func main() {
+
+	var objects []*gg.DrawnObject
+
+	gg.Init(800, 600, "Wavefront Loader")
+	gg.SetCameraPosition(gg.NewPosition(0, 0.5, 2))
+	gg.Enable(gg.PointerLock, true)
+	gg.Enable(gg.FlyMode, true)
+
+	light := gg.NewLight([]float32{1, 1, 1})
+	light.Iamb = []float32{0.4, 0.4, 0.4}
+	light.Position = gg.NewPosition(-10, 100, 10)
+	light.Radius = 300
+
+	gg.SetDirPath("github.com/seemywingz/gg/examples/assets/models/trex")
+	// model is from: https://www.blendswap.com/blends/view/89437
+	mesh := gg.LoadObject("trex.obj")
+	obj := gg.NewMeshObject(gg.Position{}, mesh, gg.Shader["phong"])
+	obj.SceneLogic = func(s *gg.SceneData) {
+		s.YRotation++
+	}
+	objects = append(objects, obj)
+
+	for !gg.ShouldClose() {
+		gg.Update()
+		for _, o := range objects {
+			o.Draw()
+		}
+		gg.SwapBuffers()
+	}
+}
