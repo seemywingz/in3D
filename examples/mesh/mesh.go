fbf0075a86b92f4c928641e51d426a0ad73ef5e5
Kevin Jayne <Kevin.Jayne@ADP.com>
2017-09-26 16:28:28 -0400

    udate mesh

--- examples/mesh/mesh.go
+++ examples/mesh/mesh.go
@@ -1,11 +1,3 @@
-# GG
-A relatively simple Go powered OpenGL Graphics Engine
-
-Create a new Window, Get OpenGL Context, Setup Camera Projection,  
-create 3D gopher from an .obj file, Draw!  
-Go Ahead, you can do it yourself...
-`go get github.com/seemywingz/gg`
-```go
 package main
 
 import (
@@ -47,17 +39,3 @@ func main() {
 		gg.SwapBuffers()
 	}
 }
-
-```
-### ME-TODO:
-  •  Optimize all the things!  
-  •  Add Shadows, Ambient Occulsion and other light related things  
-  • Have more fun making weird examples! 
-
-### YOU-TODO:
-Checkout the other examples to see some more basic functionality
-
-##### Note:
-###### Some Names and method may change until version 1.0 is tagged
-###### Also, texture UVs are, for some reason, imported upside down. ( flip your texture vertiacally to render correctly  )
-
