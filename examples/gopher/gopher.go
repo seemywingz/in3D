8c8004102d3d9c8a347b0246269205cc690fc51d
Kevin Jayne <Kevin.Jayne@ADP.com>
2017-09-25 22:34:06 -0400

    update gopher

--- examples/gopher/gopher.go
+++ examples/gopher/gopher.go
@@ -11,14 +11,15 @@ func main() {
 	gg.Init(800, 600, "Wavefront Loader")
 	gg.SetCameraPosition(gg.NewPosition(0, 0, 5))
 
-	l := gg.NewLight()
-	l.Position = gg.NewPosition(-10, 10, 10)
-	l.Radius = 30
+	light := gg.NewLight([]float32{1, 1, 1})
+	light.Iamb = []float32{0.8, 0.8, 0.8}
+	light.Position = gg.NewPosition(-10, 10, 10)
+	light.Radius = 30
 
 	gg.SetDirPath("github.com/seemywingz/gg/examples/gopher")
 	gopherMesh := gg.LoadObject("gopher.obj")
 	gopher := gg.NewMeshObject(gg.Position{}, gopherMesh, gg.NoTexture, gg.Shader["phong"])
-	gopher.ZRotation = -90
+	gopher.ZRotation = -90 // this .obj was exported sideways lol
 	gopher.SceneLogic = func(s *gg.SceneData) {
 		s.YRotation++
 	}
