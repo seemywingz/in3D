6ba9ae56d6796795051a5892017a8c2275ed2815
Kevin Jayne <Kevin.Jayne@ADP.com>
2017-10-04 09:35:36 -0400

    lighting

--- examples/mesh/mesh.go
+++ examples/mesh/mesh.go
@@ -17,11 +17,11 @@ func main() {
 
 	light := in3D.NewLight()
 	light.Ambient = []float32{0.5, 0.5, 0.5}
-	light.Specular = []float32{1, 1, 1}
+	light.Specular = []float32{10, 10, 10}
 	light.Position = in3D.NewPosition(1, 0, 0)
 	light.Draw = true
-	light.DrawnObject.Scale = 0.1
-	light.Radius = 100000
+	light.DrawnObject.Scale = 0.05
+	light.Radius = 10
 
 	model := "sky"
 	in3D.SetRelPath("../assets/models/" + model)
