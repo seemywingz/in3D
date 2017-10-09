5f49c359ce88ff7ae47bcb0864ba9019b394f333
Kevin Jayne <Kevin.Jayne@ADP.com>
2017-10-09 01:49:07 -0400

    compare phong to phong normalmap

--- examples/mesh/mesh.go
+++ examples/mesh/mesh.go
@@ -18,7 +18,7 @@ func main() {
 	light := in3D.NewLight()
 	light.Ambient = []float32{0.5, 0.5, 0.5}
 	light.Specular = []float32{10, 10, 10}
-	light.Position = in3D.NewPosition(1, 1, 0)
+	light.Position = in3D.NewPosition(0, 1, 1)
 	light.Draw = true
 	light.DrawnObject.Scale = 0.05
 	light.Radius = 10
@@ -36,12 +36,20 @@ func main() {
 	meshShader := in3D.Shader["normalMap"]
 	in3D.SetRelPath("../assets/models/" + model)
 	mesh := in3D.LoadObject(model+".obj", meshShader)
-	buddha := in3D.NewMeshObject(in3D.Position{}, mesh, meshShader)
+	buddha := in3D.NewMeshObject(in3D.NewPosition(-0.5, 0, 0), mesh, meshShader)
 	buddha.SceneLogic = func(s *in3D.SceneData) {
 		s.YRotation += 0.1
 	}
 	objects = append(objects, buddha)
-	// objects = append(objects, in3D.NewPointsObject(in3D.NewPosition(1, 1, -10), in3D.Cube, in3D.NoTexture, []float32{1, 1, 1}, in3D.Shader["normalMap"]))
+
+	model = "buddha"
+	meshShader = in3D.Shader["phong"]
+	mesh = in3D.LoadObject(model+".obj", meshShader)
+	buddha = in3D.NewMeshObject(in3D.NewPosition(0.5, 0, 0), mesh, meshShader)
+	buddha.SceneLogic = func(s *in3D.SceneData) {
+		s.YRotation += 0.1
+	}
+	objects = append(objects, buddha)
 
 	for !in3D.ShouldClose() {
 		in3D.Update()
